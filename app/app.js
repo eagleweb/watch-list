ca55defc6758f60b79845632d8086950826298f0
eagleweb <eagleweb@mail.ru>
2016-10-04 23:06:12 +0300

    Start routing

--- app/app.js
+++ app/app.js
@@ -1,9 +1,22 @@
-angular.module('watch-list', ['ngRoute'])
-    .config(function($routeProvider){
-       $routeProvider
-        //страница по умолчанию
-           .whan('/',{
-               templateUrl: '../template/index.html',
-           })
-    });
+var app= angular.module('app', ['ngRoute'])
+.config(function($routeProvider){
+    $routeProvider
+    //страница по умолчанию
+        .when('/', {
+            templateUrl: '../template/index.html',
+            controller : 'Homepage'
+        })
+        .when('/about', {
+            templateUrl: '../template/about.html',
+            controller : 'About'
+        });
+});
+
+app.controller('Homepage',['$scope',function($scope){
+    $scope.homepage = "Главная";
+}]);
+app.controller('About',['$scope', function($scope){
+    $scope.about = "Lorem ipsum...";
+}]);
+
 
